f2db0c325978ced74334c1c1d0e89b383225905c
Cosmin Codrea <codrea.cosmin27@gmail.com>
2018-11-05 16:57:24 +0200

    dependencies

--- app/src/main/java/com/dagger2ex/cosmincodrea/mvvm_kotlin/page/MainActivity.kt
+++ app/src/main/java/com/dagger2ex/cosmincodrea/mvvm_kotlin/page/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dagger2ex.cosmincodrea.mvvm_kotlin
+package com.dagger2ex.cosmincodrea.mvvm_kotlin.page
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.dagger2ex.cosmincodrea.mvvm_kotlin.R
 
 class MainActivity : AppCompatActivity() {
 
